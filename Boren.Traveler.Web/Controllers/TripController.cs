1a1c8cdaeb0c8fae553fa6f6df5370de9f1dd8f8
Boren Ding <kai21413@gmail.com>
2024-08-06 17:44:32 +0800

    add trip/list

---
+++ Boren.Traveler.Web/Controllers/TripController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Boren.Traveler.Web.Controllers
+{
+    public class TripController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
